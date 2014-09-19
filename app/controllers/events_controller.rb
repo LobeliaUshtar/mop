862e5c7efef5fb57fdc1a489813727346ff33b36
Becky Osiecki <lobeliaushtar@gmail.com>
2014-09-19 19:23:23 -0400

    delete events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -29,7 +29,9 @@ class EventsController < ApplicationController
   end
 
   def destroy
-    
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_url
   end
 
   private
