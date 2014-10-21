0cf6ecc9924747d9647b063f1e83e2347c08f1aa
Becky Osiecki <lobeliaushtar@gmail.com>
2014-10-21 17:42:59 -0400

    fixing glyphicons - attempt 2

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ Bundler.require(*Rails.groups)
 
 module Mop
   class Application < Rails::Application
-    config.assets.paths << "#{Rails}/vendor/assets/fonts"
+    config.assets.paths += %W("#{Rails.root}/vendor/assets/fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
